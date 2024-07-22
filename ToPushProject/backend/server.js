c6db903282c88cbc29ae53417dc63452d335be08
EyanleroySequeira <eyanleroy444@gmail.com>
2024-07-22 12:03:41 +0530

    mongoDB connected to Atlas

--- ToPushProject/backend/server.js
+++ ToPushProject/backend/server.js
@@ -6,11 +6,11 @@ dotenv.config({ path: "././config.env" });
 
 const app = require("./app");
 
-//LOCAL DATABASE CONNECTION
-mongoose.connect(process.env.DATABASE_LOCAL).then(() =>
-  // console.log(con.connection);
-  console.log("DB connection successfull !!(localhost)")
-);
+
+const dbURI = 'mongodb+srv://eyanleroy:1234567890@cluster1.0m9bzmq.mongodb.net/miniProject?retryWrites=true&w=majority&appName=Cluster1';
+mongoose.connect(dbURI)
+  .then(() => console.log('MongoDB connected...'))
+  .catch(err => console.error('MongoDB connection error:', err));
 
 const port = 3000;
 app.listen(port, () => {
